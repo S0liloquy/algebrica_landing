e77ade1006a579a3158fcf4ee870ad75adf05179
S0liloquy <lilia.bcorreia@gmail.com>
2017-11-08 14:03:23 +0000

    menu_on_scroll

--- www.algebrica_landing-page.pt/js/scripts.js
+++ www.algebrica_landing-page.pt/js/scripts.js
@@ -1,4 +1,6 @@
-$(document).ready(function(){
+//paralax function
+
+$(document).ready(function () {
     $('.parallax').parallax();
 
        
@@ -9,12 +11,15 @@ $(document).ready(function(){
             
     })*/
        
-       // set content on click
-$('.btn-mod-border').click(function(e) {
+//togle buttons
+    
+// set content on click
+$('.btn-mod-border').click(function (e) {
     e.preventDefault();
     setContent($(this));
 });
 
+    
 // set content on load
 $('.btn-mod-border.btn-active').length && setContent($('.btn-mod-border.btn-active'));
 
@@ -27,3 +32,48 @@ function setContent($el) {
 };
         $(".button-collapse").sideNav();
            });
+
+
+//hide menu on-scroll down
+
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('h-nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('h-nav-up').addClass('h-nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
+
+
+
+
